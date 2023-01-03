e5f0023846f03874c2f6971472271f5c4713eac5
moyen75 <moyenislam75@gmail.com>
2023-01-04 01:54:17 +0600

    update many thing

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   age: Number,
   favoriteFoods: [String]
@@ -64,18 +64,28 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true }, (err, updatedDoc) => {
+    if (err) return console.log(err);
+    done(null, updatedDoc);
+  })
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(
+    personId,
+    (err, removedDoc) => {
+      if (err) return console.log(err);
+      done(null, removedDoc);
+    }
+  );
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (err, response) => {
+    if (err) return console.log(err);
+    done(null, response);
+  })
 };
 
 const queryChain = (done) => {
